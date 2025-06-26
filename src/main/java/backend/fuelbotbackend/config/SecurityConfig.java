864ec4f7c214c93127481f32a557f0377aa8c4d8
Nathanael <nathanael.reich@gmail.com>
2025-06-26 20:51:45 +0200

    to rebase

--- src/main/java/backend/fuelbotbackend/config/SecurityConfig.java
+++ src/main/java/backend/fuelbotbackend/config/SecurityConfig.java
@@ -33,7 +33,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOriginPatterns(Arrays.asList("http://localhost:4200"));
+        configuration.setAllowedOriginPatterns(Arrays.asList("http://localhost:4200", "https://fuelbot-front-production.up.railway.app"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Authorization"));
         configuration.setAllowCredentials(true);
